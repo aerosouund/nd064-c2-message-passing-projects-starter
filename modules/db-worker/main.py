dace594e4797d3e60069e609cbd030ac14c49ccd
Ammar Yasser <aerosound161@gmail.com>
2022-07-27 16:41:57 +0200

    configured db worker

--- modules/db-worker/main.py
+++ modules/db-worker/main.py
@@ -1,8 +1,13 @@
 from kafka import KafkaConsumer
 import json
 
+from .services import PersonService
+
 consumer_persons = KafkaConsumer('persons', bootstrap_servers = ['kafka:9092'],
 value_deserializer = lambda m: json.loads(m.decode('utf-8')))
 
 for message in consumer_persons:
-    pass
+    try:
+        PersonService.retrieve(message.person_id)
+    except:
+        PersonService.create(message)
