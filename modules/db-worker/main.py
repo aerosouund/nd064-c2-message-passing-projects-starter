fe854dd96e2d62ff7cf24c7369a9f1c2563e8b6d
Ammar Yasser <aerosound161@gmail.com>
2022-07-28 11:40:45 +0200

    added logging

--- modules/db-worker/main.py
+++ modules/db-worker/main.py
@@ -1,12 +1,22 @@
 from kafka import KafkaConsumer
 import json
-from .db import get_person, load_person 
+from .db import get_person, load_person
+import logging
+
+logging.basicConfig(
+    stream=sys.stdout,
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.DEBUG,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 consumer_persons = KafkaConsumer('persons', bootstrap_servers = ['kafka:9092'],
 value_deserializer = lambda m: json.loads(m.decode('utf-8')))
 
 for message in consumer_persons:
     try:
+        logging.debug('attempting to retrieve record')
         get_person(message.person_id)
+        logging.debug('record found, no need for a write')
     except:
         load_person(message)
+        logging.debug('wrote record to db')
