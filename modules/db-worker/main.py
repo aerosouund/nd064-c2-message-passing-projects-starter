87024508aa9a629bfff6838c9a7f55b1a8af749e
Ammar Yasser <aerosound161@gmail.com>
2022-07-28 11:07:56 +0200

    configured new db logic

--- modules/db-worker/main.py
+++ modules/db-worker/main.py
@@ -1,13 +1,12 @@
 from kafka import KafkaConsumer
 import json
-
-from .services import PersonService
+from .db import get_person, load_person 
 
 consumer_persons = KafkaConsumer('persons', bootstrap_servers = ['kafka:9092'],
 value_deserializer = lambda m: json.loads(m.decode('utf-8')))
 
 for message in consumer_persons:
     try:
-        PersonService.retrieve(message.person_id)
+        get_person(message.person_id)
     except:
-        PersonService.create(message)
+        load_person(message)
