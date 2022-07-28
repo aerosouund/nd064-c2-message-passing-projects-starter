29dcdda2e0819cea8cf5920bf408ef699d4997be
Ammar Yasser <aerosound161@gmail.com>
2022-07-28 11:07:26 +0200

    some db logic

---
+++ modules/db-worker/db.py
@@ -0,0 +1,34 @@
+import psycopg2
+from typing import Dict
+import os
+
+def connect():
+    conn = psycopg2.connect(
+        host=os.environ['DB_HOST'],
+        database=os.environ['DB_NAME'],
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASSWORD'])
+    return conn
+ 
+
+def get_person(person_id):
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute('SELECT id FROM person WHERE id=%s', (person_id))
+    results = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return results[0]
+    
+
+def load_person(person: Dict):
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute(
+        'INSERT INTO person (id, first_name, laft_name, company_name) VALUES (%s, %s, %s, %s)',
+        (person.id, person.first_name, person.last_name, person.company_name)
+        )
+    conn.commit()
+    cursor.close()
+    conn.close()
+    
