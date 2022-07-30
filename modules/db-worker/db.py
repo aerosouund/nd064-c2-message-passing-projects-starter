fec4c65320b70c170c8337d7f9c757835734fe2e
Ammar Yasser <aerosound161@gmail.com>
2022-07-30 15:07:13 +0200

    fixed db logic

--- modules/db-worker/db.py
+++ modules/db-worker/db.py
@@ -6,7 +6,7 @@ def connect():
     conn = psycopg2.connect(
         host=os.environ['DB_HOST'],
         database=os.environ['DB_NAME'],
-        user=os.environ['DB_USER'],
+        user=os.environ['DB_USERNAME'],
         password=os.environ['DB_PASSWORD'])
     return conn
  
