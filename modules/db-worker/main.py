e035087e5d18d8f1abf70a8de136153691f6af6b
Ammar Yasser <aerosound161@gmail.com>
2022-08-07 16:50:37 +0200

    fixing

--- modules/db-worker/main.py
+++ modules/db-worker/main.py
@@ -17,7 +17,7 @@ consumer_persons.subscribe(topics='persons')
 
 
 for message in consumer_persons:
-    payload = json.loads(json.loads(message.value))
+    payload = message
     logging.debug('attempting to retrieve record')
     try:
         print('figuring out types', payload , type(payload))
