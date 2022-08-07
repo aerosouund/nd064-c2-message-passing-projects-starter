06575e87ec4191bd29730055130da9441bb2bb0a
Ammar Yasser <aerosound161@gmail.com>
2022-08-07 17:29:11 +0200

    fixing

--- modules/db-worker/main.py
+++ modules/db-worker/main.py
@@ -3,6 +3,7 @@ import json
 from db import get_person, load_person
 import logging
 import sys
+import ast
 
 logging.basicConfig(
     stream=sys.stdout,
@@ -17,7 +18,7 @@ consumer_persons.subscribe(topics='persons')
 
 
 for message in consumer_persons:
-    payload = json.loads(json.dumps(message.value))
+    payload = ast.literal_eval(message.value)
     logging.debug('attempting to retrieve record')
     try:
         print('figuring out types', payload , type(payload))
