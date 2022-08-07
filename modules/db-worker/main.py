1275c1b438e48712d81a250897f758d3c85e3023
Ammar Yasser <aerosound161@gmail.com>
2022-08-07 18:48:58 +0200

    added the serializer again

--- modules/db-worker/main.py
+++ modules/db-worker/main.py
@@ -11,7 +11,7 @@ logging.basicConfig(
     level=logging.DEBUG,
     datefmt='%Y-%m-%d %H:%M:%S')
 
-consumer_persons = KafkaConsumer(bootstrap_servers = ['kafka:9092'])
+consumer_persons = KafkaConsumer(bootstrap_servers = ['kafka:9092'], value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 consumer_persons.subscribe(topics='persons')
 
 
