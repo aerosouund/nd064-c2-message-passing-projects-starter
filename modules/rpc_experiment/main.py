57217a3cb58f849999f56dafcf9f59379205180a
Ammar Yasser <aerosound161@gmail.com>
2022-08-09 16:02:35 +0200

    created rpc test

--- modules/rpc_experiment/main.py
+++ modules/rpc_experiment/main.py
@@ -4,7 +4,7 @@ import locations_pb2_grpc
 
 print("Sending payload...")
 
-channel = grpc.insecure_channel("localhost:5005")
+channel = grpc.insecure_channel("udaconnect-locations:5005")
 stub = locations_pb2_grpc.LocationServiceStub(channel)
 
 response = stub.Get(locations_pb2.LocationMessage())
