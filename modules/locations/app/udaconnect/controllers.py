a771db8918ce4458a4c8c2fbac153420783ff370
Ammar Yasser <aerosound161@gmail.com>
2022-08-10 18:56:39 +0200

    debugging logs;

--- modules/locations/app/udaconnect/controllers.py
+++ modules/locations/app/udaconnect/controllers.py
@@ -26,7 +26,8 @@ api = Namespace("UdaConnect", description="Connections via geolocation.")  # noq
 class LocationResource(Resource):
     def post(self) -> Location:
         location = request.get_json()
-        app.logger.debug(f'the object received is {type(location)}, {location}')
+        logging.info(f'the object received is {type(location)}, {location}')
+        print(f'the object received is {type(location)}, {location}')
         channel = grpc.insecure_channel("rpc-server:5005")
         stub = locations_pb2_grpc.LocationServiceStub(channel)
         response = stub.Create(location)
