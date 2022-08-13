7d6828117c321a17222b58a8611bde48b29a22bf
Ammar Yasser <aerosound161@gmail.com>
2022-08-13 14:33:46 +0200

    missing imports

--- modules/persons/app/udaconnect/schemas.py
+++ modules/persons/app/udaconnect/schemas.py
@@ -1,4 +1,4 @@
-from app.udaconnect.models import Person
+from app.udaconnect.models import Person, Location
 from geoalchemy2.types import Geometry as GeometryType
 from marshmallow import Schema, fields
 from marshmallow_sqlalchemy.convert import ModelConverter as BaseModelConverter
